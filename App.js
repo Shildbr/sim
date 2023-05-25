9f55479f638acf27658a03932f669e3107b447cd
Shildbr <brunopaz.beuting@gmail.com>
2023-05-25 10:16:49 -0300

    sim

--- App.js
+++ App.js
@@ -1,44 +1,42 @@
 import React, { useRef } from 'react';
 import { View, StyleSheet, PanResponder, Dimensions } from 'react-native';
-
+import axios, { HttpStatusCode } from 'axios';
 const { width, height } = Dimensions.get('window');
 
 const App = () => {
 
     const circleRef = useRef(null);
     const circleRef2 =useRef(null);
-    const circleRef3 =useRef(null);
+    
   
     const onPanResponderMove = (evt, gestureState) => {
       const moveX = gestureState.moveX;
   
       // Limita o movimento do círculo dentro da área de 200 pixels
-      const left = Math.min(Math.max(moveX, width/2-80), width/2+80);
-  
+      const left = Math.min(Math.max(moveX, width/2-100), width/2+100);
       circleRef.current.setNativeProps({
         style: {
-          left: left - 50,
+          left: left - 25,
         }
       });
-    };
-  
+
+      const position = left-width/2+100;
+      console.log("Posição atual do círculo:", position);
+
+      axios.post('http://192.168.4.1/post', { plain: position.toString() }).then(response => {
+        console.log(response.data);
+          })
+        };
+
     const panResponder = useRef(
       PanResponder.create({
         onMoveShouldSetPanResponderCapture: () => true,
         onPanResponderMove: onPanResponderMove,
-        onPanResponderGrant: (evt, gestureState) => {
-          circleRef.current.setNativeProps({
-            style: {
-              left: gestureState.x0 - 50,
-            }
-          });
-        }
       })
     ).current;
     return (
         <View style={styles.container}>            
-            <View style={styles.circle2} ref={circleRef2}/>
-            <View style={styles.circle3} ref={circleRef3}/>
+            <View style={styles.circle3} ref={circleRef2}/>
             <View style={styles.circle} ref={circleRef} {...panResponder.panHandlers} />
 
         </View>
@@ -52,31 +50,22 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    circle2: {
-        width: 270,
-        height: 110,
-        borderRadius: 110,
-        backgroundColor: '#371636',
-        position: 'absolute',
-        left: (width / 2) - 135,
-        top: (height / 2) - 215,
-    },
     circle3: {
-        width: 258,
-        height: 100,
-        borderRadius: 110,
-        backgroundColor: '#151515',
-        position: 'absolute',
-        left: (width / 2) - 129,
-        top: (height / 2) - 210,
-    },
-    circle: {
-        width: 100,
-        height: 100,
-        borderRadius: 50,
+        width: 252,
+        height: 102,
+        borderRadius: 10,
         backgroundColor: '#3cb371',
         position: 'absolute',
-        left: (width / 2) - 130,
+        left: (width / 2) - 126,
+        top: (height / 2) - 211,
+    },
+    circle: {
+        width: 50,
+        height: 100,
+        borderRadius: 10,
+        backgroundColor: '#151515',
+        position: 'absolute',
+        left: (width / 2) - 125,
         top: (height / 2) - 210,
     },
       
